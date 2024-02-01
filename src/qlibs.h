a7f248b57519782e9b1dcc729359f60373f4e0a2
camilo <juancamilo.gomezcadavid@sensiaglobal.com>
2024-01-31 20:22:26 -0500

    bump to 1.1.4

--- src/qlibs.h
+++ src/qlibs.h
@@ -1,7 +1,7 @@
 /*!
  * @file qlibs.h
  * @author J. Camilo Gomez C.
- * @version 1.1.3
+ * @version 1.1.4
  * @note This file is part of the qlibs++ distribution.
  * @brief Global inclusion header
  **/
@@ -41,8 +41,8 @@ This file is part of the QuarkTS++ OS distribution.
 #ifndef QLIBS_CPP_H
 #define QLIBS_CPP_H
 
-#define QLIBS_CPP_VERSION         "1.1.3"
-#define QLIBS_CPP_VERNUM          ( 113U )
+#define QLIBS_CPP_VERSION         "1.1.4"
+#define QLIBS_CPP_VERNUM          ( 114U )
 #define QLIBS_CPP_CAPTION         "qLibs++" QLIBS_CPP_VERSION
 
 #include <include/qlibs_types.hpp>
