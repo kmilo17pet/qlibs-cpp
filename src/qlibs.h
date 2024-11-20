69583089f39dd3819faec9a92527a3ae688c1364
camilo <juancamilo.gomezcadavid@sensiaglobal.com>
2024-11-20 11:27:12 -0500

    bump to 1.3.3

--- src/qlibs.h
+++ src/qlibs.h
@@ -1,7 +1,7 @@
 /*!
  * @file qlibs.h
  * @author J. Camilo Gomez C.
- * @version 1.3.1
+ * @version 1.3.3
  * @note This file is part of the qlibs++ distribution.
  * @brief Global inclusion header
  **/
@@ -41,8 +41,8 @@ This file is part of the QuarkTS++ OS distribution.
 #ifndef QLIBS_CPP_H
 #define QLIBS_CPP_H
 
-    #define QLIBS_CPP_VERSION         "1.3.2"
-    #define QLIBS_CPP_VERNUM          ( 132 )
+    #define QLIBS_CPP_VERSION         "1.3.3"
+    #define QLIBS_CPP_VERNUM          ( 133 )
     #define QLIBS_CPP_CAPTION         "qLibs++" QLIBS_CPP_VERSION
 
     #include <include/qlibs_types.hpp>
@@ -62,7 +62,7 @@ This file is part of the QuarkTS++ OS distribution.
 
     namespace qlibs {
         namespace build {
-            constexpr const uint32_t number = 2351;
+            constexpr const uint32_t number = 2363;
             constexpr const char* date = __DATE__;
             constexpr const char* time = __TIME__;
             constexpr const char* std = "c++11";
@@ -72,7 +72,7 @@ This file is part of the QuarkTS++ OS distribution.
             constexpr const uint8_t number = QLIBS_CPP_VERNUM;
             constexpr const uint8_t mayor = 1U;
             constexpr const uint8_t minor = 3U;
-            constexpr const uint8_t rev = 2U;
+            constexpr const uint8_t rev = 3U;
         }
         namespace product {
             constexpr const char* author = "J. Camilo Gomez C.";
